8d35059fb12c36a122eaf1e2ae7732786fd610d3
Darren Tebo <darrentebo83@outlook.com>
2020-05-17 21:46:50 -0400

    completed the topics tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,21 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+const topicsContainer = document.querySelector('.topics');
+
+const url = 'https://lambda-times-backend.herokuapp.com/topics';
+
+// Make the api request to get the topics
+axios.get(url)
+    .then((topics) => {
+        // This code is for testing purposes. It is used to view the data returned from the api
+        console.log(topics.data.topics);
+
+        topics.data.topics.forEach((topic) => {
+            let tab = document.createElement('div');
+            tab.classList.add('tab');
+            tab.textContent = topic;
+
+            topicsContainer.appendChild(tab);
+        });
+    });
